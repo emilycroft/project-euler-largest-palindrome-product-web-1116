9f0af03abd378230a3158e8c73369e163ac7dd79
emilycroft <emilyhcroft@gmail.com>
2017-01-19 14:22:22 -0500

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,18 @@
-# Implement your procedural solution here!
+def largest_palindrome_product
+    idx1 = 999
+     idx2 = 999
+    max_palindrome = 0
+
+    while idx1 >= 100
+      idx2 = 999
+      while idx2 >= 100
+        product = idx1 * idx2
+          if product.to_s == product.to_s.reverse
+            max_palindrome = product if product > max_palindrome
+          end
+        idx2 -= 1
+      end
+      idx1 -= 1
+    end
+  max_palindrome
+end
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,27 @@
 # Implement your object-oriented solution here!
+class LargestPalindromeProduct
+  def largest_palindrome_product
+      idx1 = 999
+       idx2 = 999
+      @max_palindrome = 0
+
+      while idx1 >= 100
+        idx2 = 999
+        while idx2 >= 100
+          product = idx1 * idx2
+            if product.to_s == product.to_s.reverse
+              @max_palindrome = product if product > @max_palindrome
+            end
+          idx2 -= 1
+        end
+        idx1 -= 1
+      end
+    @max_palindrome
+  end
+
+    def answer
+      largest_palindrome_product
+    end
+
+
+end
